2700d43db8d2aba672610a046e2ded1ad938b4b3
Dhayala Ganesh <dhayal1997@gmail.com>
2021-06-29 14:36:51 +0530

    MIgration changes

--- AnyPortalBackend/AnyPortalBE/Migrations/20210619100508_AnyPortalBE.Designer.cs
+++ AnyPortalBackend/AnyPortalBE/Migrations/20210619100508_AnyPortalBE.Designer.cs
@@ -4,14 +4,16 @@ using AnyPortalBE.Models.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AnyPortalBE.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210619100508_AnyPortalBE")]
+    partial class AnyPortalBE
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
